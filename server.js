1ec0452e80232c0d4df18337d731a23713cdd325
HodRZ <hodaifa.radwan@gmail.com>
2022-08-03 22:11:25 +0300

    Add weather and movies routes

--- server.js
+++ server.js
@@ -2,41 +2,18 @@
 require('dotenv').config()
 const express = require("express")
 const app = express()
-
 const cors = require('cors');
 app.use(cors());
 const weatherData = require('./data/weather.json')
 const PORT = (process.env.PORT || 3001)
+const { weatherSearch } = require('./weather.js')
+const { movieSearch } = require('./movies.js')
 
-class Forecast {
-    constructor(data) {
-        this.description = `Low of ${data.low_temp}, high of ${data.high_temp} with ${data.weather.description}`;
-        this.date = data.datetime;
-    }
-}
 
-app.listen(PORT, () => {
-    console.log(`${PORT} Radio!`)
+app.listen(3001, () => {
+    console.log(`3001 Radio!`)
 })
 
-app.get('/weather', (req, res) => {
-    const selctedCity = {
-        lon: req.query.lon,
-        lat: req.query.lat,
-        city: req.query.searchQuery.toLowerCase()
-    }
-    const found = weatherData.find((city) => {
-        return (city.city_name.toLowerCase() === selctedCity.city)
-    })
-    const result = []
-    try {
-        found.data.forEach((day) => {
-            result.push(new Forecast(day))
-            res.send(result)
-        })
-    } catch (error) {
-        console.log('error :(')
-        res.status(500).send('Something went wrong.')
-    }
+app.get('/weather', weatherSearch)
 
-})
+app.get('/movies', movieSearch)
---
+++ weather.js
@@ -0,0 +1,29 @@
+const axios = require('axios')
+
+
+async function weatherSearch(req, res) {
+    const weatherData = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${req.query.lat}&lon=${req.query.lon}&key=${process.env.WEATHER_KEY}`)
+    try {
+        const result = []
+        weatherData.data.data.forEach((day) => {
+            result.push(new Forecast(day))
+        })
+        res.send(result)
+    } catch (error) {
+        console.log(error, 'error :(')
+        res.status(500).send('Something went wrong.')
+    }
+}
+
+
+
+class Forecast {
+    constructor(data) {
+        this.description = `Low of ${data.min_temp}, high of ${data.max_temp} with ${data.weather.description}`;
+        this.date = data.valid_date;
+    }
+}
+
+module.exports = { weatherSearch }
+
+// http://localhost:3001/weather?lon=35.9239625&lat=31.9515694&searchQuery=amman
